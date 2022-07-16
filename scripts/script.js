41d252cb0945c5446527f030ba58d0cc5a10c25c
Salihler <eren.salihler@gmail.com>
2022-07-16 21:29:38 +0300

    finalize

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,110 @@
-window.onload = function () {
-    var object = {
-        mainmenu: [
-            "English / Türkçe",
-            "Projeler",
-            "Sosyal Medya",
-            "İletişim"
-        ],
-        projects: [
-            likedisc = {
-                name: "Like Disc (Not Live Yet)",
-                link: "http://www.likedisc.com/"
-            },
-            networkchanger = {
-                name: "Network Changer (Windows App)",
-                link : "https://github.com/Salihler/erensalihlercom"
+const object = {
+    mainmenu: [
+        projects = {
+            index: 1,
+            menu: "mainmenu",
+            submenu: "projects",
+            names: {
+                english: "Projects",
+                turkish: "Projeler"
             }
-        ],
-        socialmedia: [
-            github = {
-                name: "github",
-                link: "https://github.com/Salihler"
-            },
-            linkedin =
-            {
-                name: "linkedin",
-                link: "https://www.linkedin.com/in/eren-salihler/"
-            },
-            twitch = {
-                name: "twitch",
-                link: "https://www.twitch.tv/diskoking"
+        },
+        socials = {
+            index: 2,
+            menu: "mainmenu",
+            submenu: "socialmedia",
+            names: {
+                english: "Socials",
+                turkish: "Sosyal Medya"
             }
-        ],
-        contact: [
-            email = 
-            {
-                name: "E-Posta",
-                link: "eren.salihler@gmail.com"
+        },
+        contact = {
+            index: 3,
+            menu: "mainmenu",
+            submenu: "contact",
+            names: {
+                english: "Contact",
+                turkish: "İletişim"
             }
-        ]
-    };
-
-    var terminal = document.getElementById("rows");
-
-    MainMenu(terminal, object);
+        }
+    ],
+    projects: [
+        likedisc = {
+            index: 1,
+            menu: "projects",
+            name: "Like Disc (Not Live Yet)",
+            link: "http://www.likedisc.com/"
+        },
+        networkchanger = {
+            index: 2,
+            menu: "projects",
+            name: "Network Changer (Windows App)",
+            link: "https://github.com/Salihler/erensalihlercom"
+        }
+    ],
+    socialmedia: [
+        github = {
+            index: 1,
+            menu: "socialmedia",
+            name: "Github",
+            link: "https://github.com/Salihler"
+        },
+        linkedin =
+        {
+            index: 2,
+            menu: "socialmedia",
+            name: "Linkedin",
+            link: "https://www.linkedin.com/in/eren-salihler/"
+        },
+        twitch = {
+            index: 3,
+            menu: "socialmedia",
+            name: "Twitch",
+            link: "https://www.twitch.tv/diskoking"
+        }
+    ],
+    contact: [
+        email =
+        {
+            index: 1,
+            menu: "contact",
+            name: "E-Posta",
+            link: "mailto:eren.salihler@gmail.com"
+        }
+    ]
 };
 
-function MainMenu(terminal, object) {
+const mainmenu = document.getElementById("mainmenu");
+const submenus = document.getElementById("submenus");
+var language = 1;
+
+window.onload = function () {
+    MainMenu();
+};
+
+function MainMenu() {
+    mainmenu.innerHTML = "";
     object.mainmenu.forEach(element => {
-        const link = document.createElement("button");
+        const link = document.createElement("a");
         const row = document.createElement("div");
         row.setAttribute("class", "row");
-
-        link.innerHTML = element;
-        link.setAttribute("class", "screen-button")
-        link.setAttribute("onclick", "CreateLinks()")
-
+        if (language == 1) {
+            link.innerHTML = element.names.turkish;
+        }
+        else {
+            link.innerHTML = element.names.english;
+        }
+        link.setAttribute("class", "screen-button");
+        link.setAttribute("id", element.menu + element.index);
+        link.setAttribute("onclick", "CreateLinks(\"" + element.submenu + "\")");
         row.appendChild(link);
-        terminal.appendChild(row);
+        mainmenu.appendChild(row);
     });
 }
 
-function CreateLinks(terminal, object) {
-    object.forEach(element => {
+function CreateLinks(menu) {
+    submenus.innerHTML = "";
+    object[menu].forEach(element => {
         const link = document.createElement("a");
         const row = document.createElement("div");
         row.setAttribute("class", "row");
@@ -72,6 +115,24 @@ function CreateLinks(terminal, object) {
         link.setAttribute("type", "button");
 
         row.appendChild(link);
-        terminal.appendChild(row);
+        submenus.appendChild(row);
     });
+}
+
+function ChangeLang(lang) {
+    language = lang;
+    MainMenu();
+}
+
+function Funny() {
+    if (language == 1) {
+        if (window.confirm("Gitmek istediğine emin misin?")) {
+            window.location.href = "https://www.ismycomputeronfire.com/";
+        }
+    }
+    else {
+        if (window.confirm("Do you really want to leave?")) {
+            window.location.href = "https://www.ismycomputeronfire.com/";
+        }
+    }
 }
